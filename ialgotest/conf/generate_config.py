13a2471cfd61a8eb885c0345b68d54b3529be04b
fs714 <fs714@163.com>
2017-03-28 00:29:07 +0800

    Update generate_config.py since rqalpha upgrage to 2.0.0 beta

--- ialgotest/conf/generate_config.py
+++ ialgotest/conf/generate_config.py
@@ -1,11 +1,16 @@
 def gen_conf(strategy_file, plot=False, save_plot=False, save_output=False, report_path=None):
     config = {
+        'version': '0.1.5',
+        # 白名单，设置可以直接在策略代码中指定哪些模块的配置项目
         'whitelist': ['base', 'extra', 'validator', 'mod'],
         'base': {
-            'run_id': 9999,
+            # 启动的策略文件路径
             'strategy_file': strategy_file,
+            # 数据源所存储的文件路径
             'data_bundle_path': '/root/.rqalpha/bundle',
+            # 回测起始日期
             'start_date': '2014-01-01',
+            # 回测结束日期(如果是实盘，则忽略该配置)
             'end_date': '2016-01-01',
             # 目前支持 `1d` (日线回测) 和 `1m` (分钟线回测)
             'frequency': '1d',
@@ -13,23 +18,20 @@ def gen_conf(strategy_file, plot=False, save_plot=False, save_output=False, repo
             'stock_starting_cash': 100000.0,
             # 期货起始资金
             'future_starting_cash': 0,
-            # 设置手续费乘数，默认为1
-            'commission_multiplier': 1,
             # 设置保证金乘数，默认为1
             'margin_multiplier': 1,
-            # 设置滑点
-            'slippage': 0,
             # Benchmark，如果不设置，默认没有基准参照
-            'benchmark': '000001.XSHE',
+            'benchmark': '000300.XSHG',
             # 运行类型，`b` 为回测，`p` 为模拟交易, `r` 为实盘交易
             'run_type': 'b',
-            # 设置策略类型，目前支持 `stock` (股票策略)、`future` (期货策略)及 `stock_future` (混合策略)
-            'strategy_type': 'stock',
-            # 启用的回测引擎，目前支持 `current_bar` (当前Bar收盘价撮合) 和 `next_bar` (下一个Bar开盘价撮合)
-            'matching_type': 'current_bar',
+            # 设置策略可交易品种，目前支持 `stock` (股票策略)、`future` (期货策略)tst
+            'securities': ['stock'],
+            # 在模拟交易和实盘交易中，RQAlpha支持策略的pause && resume，该选项表示开启 resume 功能
             'resume_mode': False,
+            # 在模拟交易和实盘交易中，该选项表示开启 persist功能，其会在每个bar结束对进行策略的持仓、账户信息，用户的代码上线文等内容进行持久化
             'persist': False,
             'persist_mode': 'real_time',
+            # 选择是否开启自动处理, 默认不开启
             'handle_split': False
         },
         'extra': {
@@ -47,66 +49,37 @@ def gen_conf(strategy_file, plot=False, save_plot=False, save_output=False, repo
             # 开启该项，当持仓股票退市时，按照退市价格返还现金
             'cash_return_by_stock_delisted': False,
             # 在执行order_value操作时，进行实际下单数量的校验和scale
-            'close_amount': True,
-            # 在处于涨跌停时，无法买进/卖出
-            'bar_limit': True
+            'close_amount': True
         },
         'mod': {
-            # 回测 / 模拟交易 支持 Mod
-            'simulation': {
-                'lib': 'rqalpha.mod.simulation',
+            'sys_simulation': {
                 'enabled': True,
-                'priority': 100
+                'commission_multiplier': 1.0,
+                'slippage': 0.0,
+                'matching_type': 'next_bar',
+                'signal': False
             },
-            # 技术分析API
-            'funcat_api': {
-                'lib': 'rqalpha.mod.funcat_api',
-                'enabled': False,
-                'priority': 200
-            },
-            # 开启该选项，可以在命令行查看回测进度
-            'progress': {
-                'lib': 'rqalpha.mod.progress',
-                'enabled': False,
-                'priority': 400
-            },
-            # 接收实时行情运行
-            'simple_stock_realtime_trade': {
-                'lib': 'rqalpha.mod.simple_stock_realtime_trade',
-                'persist_path': './persist/strategy/',
-                'fps': 3,
-                'enabled': False,
-                'priority': 500
-            },
-            # 渐进式输出运行结果
-            'progressive_output_csv': {
-                'lib': 'rqalpha.mod.progressive_output_csv',
-                'enabled': False,
-                'output_path': './',
-                'priority': 600
-            },
-            'risk_manager': {
-                'lib': 'rqalpha.mod.risk_manager',
+            'sys_analyser': {
                 'enabled': True,
-                'priority': 700,
-                # available_cash: 查可用资金是否充足，默认开启
-                'available_cash': True,
-                # available_position: 检查可平仓位是否充足，默认开启
-                'available_position': True,
-                # 允许裸卖空
-                'short_stock': False
-            },
-            'analyser': {
-                'priority': 100,
-                'enabled': True,
-                'lib': 'rqalpha.mod.analyser',
-                'record': True,
                 'output_file': (strategy_file.split('.')[0] + '.output') if save_output else None,
                 'plot': plot,
                 'plot_save_file': (strategy_file.split('.')[0] + '_plot') if save_plot else None,
                 'report_save_path': report_path
+            },
+            'sys_progress': {
+                'enabled': True,
+                'show': False
+            },
+            'sys_risk': {
+                'enabled': True,
+                'validate_position': True
+            },
+            'sys_stock_realtime': {
+                'enabled': False
+            },
+            'sys_funcat': {
+                'enabled': False
             }
-        },
-        'version': '0.1.2'
+        }
     }
     return config
